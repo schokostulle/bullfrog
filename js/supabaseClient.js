a990ca14a5d71a4cd4dbcf064d0abb015a5c9d23
krake <schokostulle.777@gmail.com>
2026-09-18 13:10:06 +0200

    Aktualisieren von supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -6,7 +6,7 @@
  * Diese Datei ist bewusst die einzige Stelle mit Projekt-Zugangsdaten,
  * damit der Rest des Codes unabhängig vom konkreten Projekt bleibt.
  */
-const SUPABASE_URL = "https://lllltsvxtrikvrqjousl.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://lllltsvxtrikvrqjousl.supabase.co/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbGx0c3Z4dHJpa3ZycWpvdXNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MTI0OTcsImV4cCI6MjEwNTI4ODQ5N30.AUvBESGujSqTZC5y-8jR-duhJ4vpWYRZUWO711gBjnE";
 
 // Domain, die für die "Fake-E-Mail"-Registrierung genutzt wird.
